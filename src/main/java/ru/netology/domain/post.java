e631b9f23389386a420e0077500dc60dff077b96
Aleksandr Zarubin <aljkeo.O@gmail.com>
2020-07-26 14:35:41 +0300

    v1

---
+++ src/main/java/ru/netology/domain/post.java
@@ -0,0 +1,14 @@
+package ru.netology.domain;
+
+public class post {
+    private String id;
+    private String name;
+    private String date;
+    private String imageUrl;
+    private String text;
+    private String likesInfo;
+    private String repostsInfo;
+    private String viewsInfo;
+    private String commentsInfo;
+    private String geolocation;
+}
